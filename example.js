46498f7d8aa9727f3a5847b6b725b6e689b40261
Emil Bay <github@tixz.dk>
2019-02-22 21:51:11 +0100

    Add proper example

--- example.js
+++ example.js
@@ -1,8 +1,17 @@
-var pledge = require('.')
+const pledge = require('.')
+const fs = require('fs')
+const path = require('path')
 
-console.log('before')
+const buf = Buffer.alloc(32)
+const fdFirst = fs.openSync(path.join(__dirname, 'first.txt'), 'as+', 0o555)
+
 pledge('stdio')
-console.log('after')
+// Cannot open new file descriptors now
+
+fs.readSync(fdFirst, buf, 0, 11, 0)
+fs.writeSync(fdFirst, Buffer.from('Hello world'), 0)
 
 pledge('')
-setTimeout(() => console.log('killed'), 1000)
+// Reading or writing here is illegal
+
+fs.closeSync(fdFirst)
