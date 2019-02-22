91caa63e523b9293931d66f0a52e516a0719addc
Emil Bay <github@tixz.dk>
2019-02-22 21:23:55 +0100

    Add execpromise arg

--- binding.c
+++ binding.c
@@ -51,7 +51,34 @@ NAPI_METHOD(napi_openbsd_pledge) {
     }
   }
 
-  int err = pledge((const char*) promise, NULL);
+  size_t execpromise_len = 0;
+  char execpromise[MAX_STR_LEN + 1];
+  if (argc > 1) {
+    NAPI_STATUS_THROWS(napi_typeof(env, argv[1], &arg_type));
+
+    switch (arg_type) {
+      case napi_null:
+      case napi_undefined:
+      case napi_string:
+        break;
+      default:
+        napi_throw_error(env, "EINVAL", "execpromise must be string or null");
+        return NULL;
+    }
+
+    if (arg_type == napi_string) {
+      NAPI_UTF8_GET_LENGTH(execpromise_len, argv[1]);
+
+      if (execpromise_len > MAX_STR_LEN) {
+        napi_throw_error(env, "EINVAL", "execpromise string too long");
+        return NULL;
+      }
+
+      NAPI_STATUS_THROWS(napi_get_value_string_utf8(env, argv[1], execpromise, MAX_STR_LEN + 1, &execpromise_len));
+    }
+  }
+
+  int err = pledge(promise_len > 0 ? promise : NULL, execpromise_len > 0 ? execpromise : NULL);
   if (err < 0) {
     napi_throw_error(env, err_name(errno), strerror(errno));
     return NULL;
